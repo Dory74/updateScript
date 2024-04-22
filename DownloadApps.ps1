233cb65a24f5bac3ce989273ce69214d51841b36
Blake Smith <balke2026@gmail.com>
2024-04-23 09:25:05 +1200

    Added Download script
    Added in the download script for installing chrome, office, hp support assistant, and zoom via powershell.

---
+++ DownloadApps.ps1
@@ -0,0 +1,19 @@
+﻿# Replace "X:\path\to\installer.msi" with the actual path to your MSI file
+$privatePath = "\\seeka-360-fs1\private\Blake-LaptopBuilds"
+$zoomPath = "$privatePath\ZoomInstallerFull.msi"
+$officePath = "\\SEEKA-HQDC1\dfs\clientapps\Office365\Online Installer\OfficeSetup.exe"
+$chromePath = "$privatePath\ChromeSetup.exe"
+$supportAssistantPath = "$privatePath\supportAssistantInstaller.exe"
+
+# Use msiexec.exe to install the MSI silently
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$zoomPath`" /qn" -NoNewWindow -Wait
+#Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$privatePath\ZoomInstallerFull.msi`" /qn" -NoNewWindow -Wait
+
+#Start-Process -FilePath $officePath -ArgumentList "/quiet" -NoNewWindow -Wait
+#Start-Process -FilePath $chromePath -ArgumentList "/quiet" -NoNewWindow -Wait
+#Start-Process -FilePath $supportAssistantPath -ArgumentList "/quiet" -NoNewWindow -Wait
+
+
+Start-Process -FilePath $chromePath -ArgumentList -Wait
+Start-Process -FilePath $supportAssistantPath -ArgumentList -Wait
+Start-Process -FilePath $officePath -ArgumentList -Wait
