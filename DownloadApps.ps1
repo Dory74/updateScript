8668f0fa0eafd8d3b801c0248200f7d6356b210e
Blake Smith <balke2026@gmail.com>
2024-04-23 14:33:59 +1200

    Changed to MSI
    Chaanged chrome install to an msi, and hpsupport now is silent

--- DownloadApps.ps1
+++ DownloadApps.ps1
@@ -1,5 +1,5 @@
 ﻿
-$privatePath = "\\seeka-360-fs1\private\Blake-LaptopBuilds"
+$privatePath = "\\seeka-360-fs1\private\Blake-LaptopInstallers"
 $zoomPath = "$privatePath\ZoomInstallerFull.msi"
 $officePath = "\\SEEKA-HQDC1\dfs\clientapps\Office365\Online Installer\OfficeSetup.exe"
 $chromePath = "$privatePath\ChromeSetup.exe"
@@ -7,12 +7,10 @@ $supportAssistantPath = "$privatePath\supportAssistantInstaller.exe"
 
 # Use msiexec.exe to install the MSI silently
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$zoomPath`" /qn" -NoNewWindow
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$chromePath`" /qn" -NoNewWindow
+Start-Process -FilePath $supportAssistantPath -ArgumentList "/s", "/a", "/v/qn" -NoNewWindow
 
-Write-Host "Why wont this script update raw????"
-Write-Host "Chrome Started"
-Start-Process -FilePath $chromePath -NoNewWindow 
-Write-Host "Support Assistant Started"
-Start-Process -FilePath $supportAssistantPath -NoNewWindow 
-Write-Host "Office Started"
+
+Write-Host "Why wont this script update raw????7"
 Start-Process -FilePath $officePath -NoNewWindow 
 Write-Host "Office Finished"
